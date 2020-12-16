bba2e86ab4fec3f01b6498a600b9c3c5b1cc6745
Loren Gordon <lorengordon@users.noreply.github.com>
2020-12-16 15:16:35 -0800

    Adds test to exercise all github repo variables

--- tests/create_repo/main.tf
+++ tests/create_repo/main.tf
@@ -1,4 +1,45 @@
-provider "github" {}
+module "repo" {
+  source = "../../"
+
+  name        = "test-terraform-github-repo-${random_string.this.result}"
+  description = "description for random-${random_string.this.result}"
+
+  allow_merge_commit     = true
+  allow_squash_merge     = true
+  allow_rebase_merge     = true
+  archive_on_destroy     = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = null
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = true
+  homepage_url           = "https://some/url"
+  license_template       = "apache-2.0"
+  is_template            = false
+  topics                 = ["test"]
+  visibility             = "public"
+
+  branch_protection = {
+    enforce_admins         = true
+    push_restrictions      = []
+    require_signed_commits = false
+
+    required_status_checks = {
+      strict   = false
+      contexts = ["github-actions"]
+    }
+
+    required_pull_request_reviews = {
+      dismiss_stale_reviews           = true
+      dismissal_restrictions          = []
+      require_code_owner_reviews      = false
+      required_approving_review_count = 1
+    }
+  }
+
+  teams = []
+}
 
 resource "random_string" "this" {
   length  = 10
@@ -7,14 +48,6 @@ resource "random_string" "this" {
   special = false
 }
 
-module "create_repo" {
-  source = "../../"
-  providers = {
-    github = github
-  }
-
-  name                  = "random-${random_string.this.result}"
-  enable_merge_commits  = true
-  enable_squash_merging = true
-  enable_rebase_merging = false
+output "repo" {
+  value = module.repo
 }
